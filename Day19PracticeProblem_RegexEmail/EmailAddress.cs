29aec183dbc4c005355375e4aab51d99ac7d017f
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-29 16:50:01 +0530

    UC3 Regex Email Pushed

---
+++ Day19PracticeProblem_RegexEmail/EmailAddress.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19PracticeProblem_RegexEmail
+{
+    class EmailAddress
+    {
+        public static void Emailaddress(string name)
+        {
+            string Email = "^[a-z0-9a-z]{1,}([.#$^][a-z0-9]+)*[@][a-z]{2,}[.][a-z]{2,3}([.][a-z]{2})?$";
+            Regex regex = new Regex(Email);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid.");
+            }
+        }
+    }
+}
---
+++ Day19PracticeProblem_RegexEmail/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day19PracticeProblem_RegexEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("\n Enter Email Address : ");
+            string name = Console.ReadLine();
+            EmailAddress.Emailaddress(name);
+        }
+    }
+}
