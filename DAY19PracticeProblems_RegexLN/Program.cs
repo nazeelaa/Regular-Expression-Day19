93ab5015e0d3ae00ca63f330872f287d1d106551
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-29 14:03:49 +0530

    Regular Expression UC2 Pushed

---
+++ DAY19PracticeProblems_RegexLN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DAY19PracticeProblems_RegexLN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Write your name using 1st character should be Capital ");
+            Console.Write("\n Enter LastName Name : ");
+            string name = Console.ReadLine();
+            LastName.Lastname(name);
+        }
+    }
+}
