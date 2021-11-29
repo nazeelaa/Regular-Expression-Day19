7560adc9b99917cc9f67b5dfc2ce6dd3d180225c
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-29 16:55:39 +0530

    Regular Expression UC6 Pushed

---
+++ Day19PracticeProblems_RegexUC6/PasswordRule2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19PracticeProblems_RegexUC6
+{
+    class PasswordRule2
+    {
+        public static void Password(string name)
+        {
+            string Password = "^[A-Z][A-Za-z0-9]{7}";
+            Regex regex = new Regex(Password);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid.");
+            }
+        }
+    }
+}
---
+++ Day19PracticeProblems_RegexUC6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day19PracticeProblems_RegexUC6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your Password ex : Aa9 length atleast 8 .");
+            Console.Write("\n Enter Password : ");
+            string name = Console.ReadLine();
+            PasswordRule2.Password(name);
+        }
+    }
+}
