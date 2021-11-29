551c51c4fba5b0bdd1cc22eca378bc11c7039580
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-29 13:12:45 +0530

    Regular Expression UC1 Pushed

---
+++ DAY19PracticeProblems_Regex/FirstName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DAY19PracticeProblems_Regex
+{
+    class FirstName
+    {
+        public static void Firstname(string name)
+        {
+            string FirstName = "^[A-Z][A-Za-z]\\w{1,10}$$";
+            Regex regex = new Regex(FirstName);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid. Please Enter First name start with capital letter and maximum 10 charactors");
+            }
+        }
+    }
+}
+
---
+++ DAY19PracticeProblems_Regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DAY19PracticeProblems_Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expression Program \n");
+            Console.WriteLine("Write your name. 1st character should be Capital ");
+            Console.Write("\n Enter First Name : ");
+            string name = Console.ReadLine();
+            FirstName.Firstname(name);
+
+        }
+    }
+
+}
