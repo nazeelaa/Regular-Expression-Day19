4fb8b8e46b685d0e9b2a556872b22103b60dcffe
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-29 16:54:13 +0530

    Regular Expression UC5 Pushed

---
+++ Day19PracticeProblems_RegexPP1/PasswordRule1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19PracticeProblems_RegexPP1
+{
+    class PasswordRule1
+    {
+        public static void password(string name)
+        {
+            string Password = "^[A-Za-z0-9]{8}";
+            Regex regex = new Regex(Password);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid.");
+            }
+        }
+    }
+}
---
+++ Day19PracticeProblems_RegexPP1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day19PracticeProblems_RegexPP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your Password ex : Aa9 length atleast 8 .");
+            Console.Write("\n Enter Password : ");
+            string name = Console.ReadLine();
+            PasswordRule1.password(name);
+        }
+    }
+}
