2882359e41510aec6f2b382e64aac7f12b54681e
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-29 16:57:35 +0530

    Regular Expression UC7 Pushed

---
+++ Day19PracticeProblems_RegexUC7/Password.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19PracticeProblems_RegexUC7
+{
+    class Password
+    {
+        public static void password(string name)
+        {
+            string Password = "^[A - Za-z0-9]{8}";
+            Regex regex = new Regex(Password);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid.");
+            }
+        }
+    }
+}
+
---
+++ Day19PracticeProblems_RegexUC7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day19PracticeProblems_RegexUC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter Password containig atleast 1 Number");
+            Console.WriteLine("enter the password");
+            string name = Console.ReadLine();
+            Password.password(name);
+        }
+    }
+}
