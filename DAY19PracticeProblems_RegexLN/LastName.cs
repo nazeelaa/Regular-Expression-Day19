4d34bc49caaec845d7cece133f0540a839b3632b
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-29 14:08:23 +0530

    Regular Expression UC2 Pushed

---
+++ DAY19PracticeProblems_RegexLN/LastName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DAY19PracticeProblems_RegexLN
+{
+    class LastName
+    {
+        public static void Lastname(string name)
+        {
+            string LastName = "^[A-Z][A-Za-z]\\w{1,10}$$";
+            Regex regex = new Regex(LastName);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid. Please Enter First name start with capital letter and maximum 10 charactors");
+            }
+        }
+    }
+}
