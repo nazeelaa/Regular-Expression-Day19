384b000d21af9882e108f4ba4775775bbdc39a7d
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-29 16:52:41 +0530

    Regular Expression UC4 Pushed

---
+++ Day19PracticeProblems_RegexMobileNo/MobileNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19PracticeProblems_RegexMobileNo
+{
+    class MobileNumber
+    {
+        public static void Mobilenumber(string name)
+        {
+            string MobileNumber = "(0|91)?[ ][6-9][0-9]{9}";
+            Regex regex = new Regex(MobileNumber);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid.");
+            }
+        }
+    }
+}
---
+++ Day19PracticeProblems_RegexMobileNo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day19PracticeProblems_RegexMobileNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your Mobile Number ex : +91 7563214879 .");
+            Console.Write("\n Enter Mobile Number : ");
+            string name = Console.ReadLine();
+            MobileNumber.Mobilenumber(name);
+        }
+    }
+}
+ 
